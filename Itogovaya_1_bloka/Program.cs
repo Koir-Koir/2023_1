a3a89252f481a170ff3301d1be78f3c5f85c3000
Koir-Koir <DanilovDenisrune@ya.ru>
2023-12-05 03:17:01 +0500

    Commit_4__Add_ReadInt,_FormNewMassivStrLessX,_Print1xMassivString

--- Itogovaya_1_bloka/Program.cs
+++ Itogovaya_1_bloka/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание итоговой аттестации первого блока.
+ // Массивы строк
+ // Задача: Написать программу, которая из имеющегося массива строк
+ // формирует новый массив из строк, длина которых меньше, либо равна
+ // 3 символам. Первоначальный массив можно ввести с клавиатуры, либо
+ // задать на старте выполнения алгоритма. При решении не рекомендуется
+ // пользоваться коллекциями, лучше обойтись исключительно массивами.
+//
+
+// Ошибки до компиляции
+ // 
+ // да/нет - написал без ошибок/с ошибками
+ // 
+// 
+
+// Решение - код программы
+
+using System;
+
+class Program{
+    static public void Main(string[] args){
+
+            // Функция для ввода целого числа
+        int ReadInt (string message)                
+            {Console.Write(message);                
+            string value = Console.ReadLine();      
+            return int.Parse(value); 
+        }
+
+            // Функция проверки элементов массива на определённую длинну 
+            // и генерации нового масссива из них
+        string[] FormNewMassivStrLessX(string[] oldMassiv, int stringLengthLess){
+            string[] newMassiv = new string[0];
+            for (int i = 0; i < oldMassiv.Length, i++){
+                if (oldMassiv[i].Length < stringLengthLess){
+                    int length = newMassiv.Length;
+                    Array.Resize(ref newMassiv, length + 1);
+                    newMassiv[length] = oldMassiv[i];
+                }
+            }
+            return newMassiv;
+        }
+
+            // Функция вывода одномерногомассива на экран с объявлением вывода 
+        void Print1xMassivString(string[] massivPrint, string message){
+            Console.WriteLine($"\n {message}");
+            for (int i = 0; i < massivPrint.Length; i++){
+                Console.Write($"{massivPrint[i]}\t");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
